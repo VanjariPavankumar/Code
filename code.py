dc5bdd1ac2cfa29da61b90646bab004eae82b234
VANJARI <pavan-kumar.vanjari@capgemini.com>
2024-01-03 21:06:47 +0530

    Commiting the code

---
+++ code.py
@@ -0,0 +1,16 @@
+def my_function():
+    print("hello")
+    print("bye")
+my_function()
+
+
+
+def my_function1():
+    sky = "clear"
+    if sky == "clear":
+        print("blue")
+    elif sky == "Cloudy":
+        print("grey")
+    else:
+        print("hello world!")
+my_function1()
